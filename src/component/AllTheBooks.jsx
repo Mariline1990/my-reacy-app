51891c0440d1c6f755303be4e0dc26e00b9ddac5
Mariline Luccisano <y.marilineluccisano@outlook.it>
2023-12-19 18:33:54 +0100

    Con Bottoni carini

--- src/component/AllTheBooks.jsx
+++ src/component/AllTheBooks.jsx
@@ -1,29 +1,23 @@
 import fantasy from "../assets2/fantasy.json";
-import { Card, Container, ListGroup, Row, Col } from "react-bootstrap";
+import { Card, Container, Row, Col, Button } from "react-bootstrap";
 
 const AllTheBooks = () => {
   return (
     <Container>
       <Row>
         {fantasy.map((figurina, index) => (
-          <Col key={index} className="d-flex aling-items-stretch">
+          <Col key={index} className="d-flex aling-items-auto">
             <Card style={{ width: "18rem", marginBottom: "30px", marginTop: "50px" }}>
               <Card.Img variant="top" src={figurina.img} />
               <Card.Body>
                 <Card.Title>{figurina.title}</Card.Title>
-                <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of the card's content.
-                </Card.Text>
+                <Button className="bg-success">{figurina.category}</Button>
               </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>Cras justo odio</ListGroup.Item>
-                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-              </ListGroup>
+
               <Card.Body>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
+                <Button>Acquista</Button>
               </Card.Body>
+              <strong>{figurina.price}$</strong>
             </Card>
           </Col>
         ))}
