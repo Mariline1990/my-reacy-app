5d48f9d1d405b84ac00a938f8bfeb4fe5dc01e81
Mariline Luccisano <y.marilineluccisano@outlook.it>
2023-12-19 17:58:13 +0100

    in row

--- src/component/AllTheBooks.jsx
+++ src/component/AllTheBooks.jsx
@@ -1,27 +1,36 @@
 import fantasy from "../assets2/fantasy.json";
-import { Card, ListGroup } from "react-bootstrap";
+import { Card, Container, ListGroup, Row, Col } from "react-bootstrap";
 
 const AllTheBooks = () => {
-  return fantasy.map((figurina) => (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src={figurina.img} />
-      <Card.Body>
-        <Card.Title>{figurina.title}</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the bulk of the card's content.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>Cras justo odio</ListGroup.Item>
-        <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-        <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">Card Link</Card.Link>
-        <Card.Link href="#">Another Link</Card.Link>
-      </Card.Body>
-    </Card>
-  ));
+  return (
+    <Container>
+      <Row>
+        {fantasy.map((figurina) => (
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={figurina.img} />
+              <Card.Body>
+                <Card.Title>{figurina.title}</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up the bulk of the card's content.
+                </Card.Text>
+              </Card.Body>
+              <ListGroup className="list-group-flush">
+                <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+              </ListGroup>
+              <Card.Body>
+                <Card.Link href="#">Card Link</Card.Link>
+                <Card.Link href="#">Another Link</Card.Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+        ;
+      </Row>
+    </Container>
+  );
 };
 
 export default AllTheBooks;
